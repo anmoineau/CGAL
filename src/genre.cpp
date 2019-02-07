0e68af65d465aa751a162dfa815dab3772b00f72
Antoine Moineau <moineau.antoine@gmail.com>
2019-02-07 12:05:20 +0100

    code amélioré

--- src/genre.cpp
+++ src/genre.cpp
@@ -12,7 +12,8 @@ typedef Polyhedron::Vertex_iterator Vertex_iterator;
 typedef Polyhedron::Halfedge_iterator Halfedge_iterator;
 typedef Polyhedron::Halfedge_around_facet_circulator Halfedge_facet_circulator;
 
-void draw(Polyhedron & P, std::map<Polyhedron::Facet_handle, double> perimetres, float min, float max){
+void Draw(Polyhedron & P, std::map<Polyhedron::Facet_handle, int> categories){
+	//liste de couleurs aleatoire de taille nombre de categorie -> Couleurs[categories[i]]
 	std::fstream file;
 	file.open("../Rendu.off");
 	file << "OFF" << std::endl << P.size_of_vertices() << ' ' << P.size_of_facets() << " 0" << std::endl;
@@ -24,10 +25,53 @@ void draw(Polyhedron & P, std::map<Polyhedron::Facet_handle, double> perimetres,
         do {
             file << ' ' << std::distance(P.vertices_begin(), j->vertex());
         } while ( ++j != i->facet_begin());
-		file<<"  "<<"1.000"<<' '<<(perimetres[i]-min)/((min == max)? 1.000 : max-min)<<' '<<(perimetres[i]-min)/((min == max)? 1.000 : max-min)<<' '<<"0.75"<< std::endl;
+		file<<"  "<<((categories[i] == 1)? 0.25 : 0.75)<<' '<<((categories[i] == 2)? 1.000 : 0.000)<<' '<<"1.000"<<' '<<"0.75"<< std::endl;
         file << std::endl;
 	}
 }
+
+std::map<Polyhedron::Facet_handle, double> ComputePerimetre(Polyhedron & P,float & min,float & max, float & mean){
+	max = 0;
+	min = 1000;
+	float sum = 0;
+	std::map<Polyhedron::Facet_handle, double> perimetres;
+	for (Facet_iterator i = P.facets_begin(); i != P.facets_end(); ++i) {
+		float perimetre = 0;
+		Halfedge_facet_circulator j = i->facet_begin();
+		CGAL_assertion( CGAL::circulator_size(j) >= 3);
+		do {
+            perimetre += CGAL::squared_distance(j->vertex()->point(),j->opposite()->vertex()->point());
+        } while ( ++j != i->facet_begin());
+		if(perimetre>max)
+			max = perimetre;
+		if(perimetre<min)
+			min = perimetre;
+		perimetres[i] = perimetre; 
+		sum += perimetre;
+	}
+	mean = sum/perimetres.size();
+	std::cout << "Perimetre moyen: " << mean << std::endl;
+	return perimetres;
+}
+
+std::map<Polyhedron::Facet_handle, int> ComputeTreshold(Polyhedron & P, std::map<Polyhedron::Facet_handle, double> & perimetres, const float & mean){
+	std::map<Polyhedron::Facet_handle, int> categories;
+
+    for (Facet_iterator i = P.facets_begin(); i != P.facets_end(); ++i) {
+		categories[i] = (perimetres[i]> mean)? 1 : 2;
+	}
+	return categories;
+}
+
+/*std::map<Polyhedron::Facet_handle, int> ModifyCategorie(Polyhedron & P, std::map<Polyhedron::Facet_handle, int> & categories){
+	std::map<Polyhedron::Facet_handle, bool> visite;
+	//parcours face -> visité
+	//faire le tour des arretes -> empiler facet associée si non visitée 
+	//attribuer nouvelle categorie
+	//jusqu'a pile vide
+	//partir d'une autre face non visisté
+}*/
+
 
 int main(int argc, char* argv[])
 {
@@ -56,30 +100,18 @@ int main(int argc, char* argv[])
 	std::cout << "Nombre d'arêtes: " << nbEdges << std::endl;
 
 	unsigned int nbFaces = 0;
-	float max = 0;
-	float min = 1000;
-	std::map<Polyhedron::Facet_handle, double> perimetres;
 	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i) {
 		++nbFaces;
-		float perimetre = 0;
-		Halfedge_facet_circulator j = i->facet_begin();
-		CGAL_assertion( CGAL::circulator_size(j) >= 3);
-		do {
-            perimetre += CGAL::squared_distance(j->vertex()->point(),j->opposite()->vertex()->point());
-        } while ( ++j != i->facet_begin());
-		if(perimetre>max)
-			max = perimetre;
-		if(perimetre<min)
-			min = perimetre;
-		perimetres[i] = perimetre; 
 	}
 	std::cout << "Nombre de faces: " << nbFaces << std::endl;
-	
 	unsigned int euler = nbVerts - nbEdges + nbFaces;
 	unsigned int genus = (2 - euler) / 2;
 	std::cout << "En supposant que le maillage contienne une unique surface sans bord, alors son genre est de " << genus << std::endl;
 	
-	draw(mesh, perimetres, min, max);
+	float min, max, mean;
+	std::map<Polyhedron::Facet_handle, double> perimetres = ComputePerimetre(mesh,min, max, mean);
+	std::map<Polyhedron::Facet_handle, int> categories = ComputeTreshold(mesh, perimetres, mean);
+	Draw(mesh, categories);
   
 	return 0;
 }
